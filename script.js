b22c9dbe2de74070dc798470fa5569a01a861d43
ani1154 <aniketpatil1154@gmail.com>
2024-08-31 22:52:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+<!-- JavaScript Code -->
+<script>
+    // Image Slider
+    let slideIndex = 0;
+    function showSlides() {
+        let slides = document.querySelectorAll(".slide");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
+    showSlides();
+
+    // Scroll-to-Top Button
+    window.onscroll = function() { scrollFunction() };
+
+    function scrollFunction() {
+        let mybutton = document.getElementById("scrollTopBtn");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    }
+</script>
